c29f9563fb432e30be8f303517751880ba7608fa
Brian Friesen <brian.friesen@gmail.com>
2023-12-22 23:30:51 -0500

    Fix warnings

--- RandomSkunk.RuntimePolyfill/System.Diagnostics.CodeAnalysis/StringSyntaxAttribute.cs
+++ RandomSkunk.RuntimePolyfill/System.Diagnostics.CodeAnalysis/StringSyntaxAttribute.cs
@@ -5,22 +5,6 @@ namespace System.Diagnostics.CodeAnalysis;
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 internal sealed class StringSyntaxAttribute : Attribute
 {
-    public StringSyntaxAttribute(string syntax)
-    {
-        Syntax = syntax;
-        Arguments = [];
-    }
-
-    public StringSyntaxAttribute(string syntax, params object?[] arguments)
-    {
-        Syntax = syntax;
-        Arguments = arguments;
-    }
-
-    public string Syntax { get; }
-
-    public object?[] Arguments { get; }
-
     public const string CompositeFormat = nameof(CompositeFormat);
 
     public const string DateOnlyFormat = nameof(DateOnlyFormat);
@@ -44,6 +28,22 @@ internal sealed class StringSyntaxAttribute : Attribute
     public const string Uri = nameof(Uri);
 
     public const string Xml = nameof(Xml);
+
+    public StringSyntaxAttribute(string syntax)
+    {
+        Syntax = syntax;
+        Arguments = [];
+    }
+
+    public StringSyntaxAttribute(string syntax, params object?[] arguments)
+    {
+        Syntax = syntax;
+        Arguments = arguments;
+    }
+
+    public string Syntax { get; }
+
+    public object?[] Arguments { get; }
 }
 
 #endif
