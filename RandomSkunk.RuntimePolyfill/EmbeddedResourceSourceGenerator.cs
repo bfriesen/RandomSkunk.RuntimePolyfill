bc9c204871f54d7bcc35b49db70cf16cf7b35c50
Brian Friesen <brian.friesen@gmail.com>
2024-01-03 15:30:12 -0500

    Add nuget package

---
+++ RandomSkunk.RuntimePolyfill/EmbeddedResourceSourceGenerator.cs
@@ -0,0 +1,35 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Reflection;
+using System.Text;
+
+namespace RandomSkunk.RuntimePolyfill;
+
+[Generator]
+public class EmbeddedResourceSourceGenerator : ISourceGenerator
+{
+    private static readonly string _resourceNamePrefix = typeof(EmbeddedResourceSourceGenerator).Namespace + ".";
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        // The source files from the shared project are included in the assembly as embedded resources.
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceNames = assembly.GetManifestResourceNames();
+
+        foreach (var resourceName in resourceNames)
+        {
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+            
+            var sourceCode = reader.ReadToEnd();
+            var sourceName = resourceName.Substring(_resourceNamePrefix.Length);
+            
+            // Add each source file to the compilation.
+            context.AddSource(sourceName, SourceText.From(sourceCode, Encoding.UTF8));
+        }
+    }
+}
